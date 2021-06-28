2070ae4f735a23c15822868641905ed7bba90f79
Concepcionflores <60276356+Concepcionflores@users.noreply.github.com>
2021-06-28 17:03:44 -0500

    actualizacion de usuarios
    se actualizo tabla usuarios

---
+++ prestamo/database/migrations/2021_06_28_174751_producto.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Producto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->id();
+            $table->string('Descripcion');
+            $table->string('foto');
+            $table->timestamps();
+            $table->string('Estado_actual_del_producto');
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producto');
+    }
+}
