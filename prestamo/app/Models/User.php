3ae6e26af7eeb259e09ee964baac5014be357690
Concepcionflores <60276356+Concepcionflores@users.noreply.github.com>
2021-06-30 12:22:38 -0500

    Roles

--- prestamo/app/Models/User.php
+++ prestamo/app/Models/User.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable
 {
 	use HasFactory;
+    use HasRoles;
 
     public $timestamps = true;
 
