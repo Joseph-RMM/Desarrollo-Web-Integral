508de1fbdf0ea6bd4883b04c7de3b002e4ced657
DaddyEdgar <utp0137638@alumno.utpuebla.edu.mx>
2021-07-06 00:19:42 -0500

    Nav bar

--- prestamo/resources/views/layouts/app.blade.php
+++ prestamo/resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-lg navbar-light bg">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('app.name', 'Laravel') }}
