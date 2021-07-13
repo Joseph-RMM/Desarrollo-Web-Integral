bf7b36188fdc43e50811281d5b5f159c6df92af2
Concepcionflores <60276356+Concepcionflores@users.noreply.github.com>
2021-07-13 01:34:57 -0500

    diseño se muestra doble

--- prestamo/resources/views/livewire/productossellers/index.blade.php
+++ prestamo/resources/views/livewire/productossellers/index.blade.php
@@ -1,3 +1,13 @@
+@extends('layouts.app')
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            @livewire('productos')
+        </div>     
+    </div>   
+</div>
+@endsection
 <div class="container">
     <div class="alert colordark" role="alert">
         <div class="row">
