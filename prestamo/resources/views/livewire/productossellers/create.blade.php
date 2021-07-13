3af99bf6eb856e8ba155da94e54098ea2ed9c20c
Concepcionflores <60276356+Concepcionflores@users.noreply.github.com>
2021-07-13 01:32:15 -0500

    index muestra fotos falta diseño

--- prestamo/resources/views/livewire/productossellers/create.blade.php
+++ prestamo/resources/views/livewire/productossellers/create.blade.php
@@ -1,48 +1,72 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>prueba</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <h4>crear products</h4>
-        <div class="row">
-            <div class="col-xl-12">
-                <form action="{{route('')}}" method="post">
-                    <div class="form-group">
-                        <label for="Nombre">nombre</label>
-                        <input type="text" class="form-group" name="Nombre" required minlength="4" >
+<!-- Modal -->
+<div wire:ignore.self class="modal fade bd-example-modal-lg" id="exampleModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="container">
+                    <div class="alert colordark" role="alert">
+                        <div class="row">
+                            <div class="col-lg-9 col-md-6 col-sm-2">
+                                <div class="usuario-white">
+                                    Agregar Producto
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="id_tiposdeproductos">id_tiposdeproductos</label>
-                        <input type="text" class="form-group" name="id_tiposdeproductos" required  >
-                    </div>
-                    <div class="form-group">
-                        <label for="Descripcion">Descripcion</label>
-                        <input type="text" class="form-group" name="Descripcion" required minlength="20" >
-                    </div>
-                    <div class="form-group">
-                        <label for="foto">foto</label>
-                        <input type="file" class="form-group" name="foto" required >
-                    </div>
-                    <div class="form-group">
-                        <label for="Estado_actual_del_producto">Estado_actual_del_productoto</label>
-                        <input type="text" class="form-group" name="Estado_actual_del_producto" required >
-                    </div>
-                    <div class="form-group">
-                        <label for="id_usuario">id_usuario</label>
-                        <input type="text" class="form-group" name="id_usuario" required >
-                    </div>
+                </div>
+                <div class="wrapper-agregarusuario">
+                    <div class="form-container">
+                        <div class="form-inner">
+                            <form class="signup">
+                                <label>Crear un nuevo Producto</label>
+                                <div class="field">
+                                    <label>Nombre <b class="rojo">*</b></label>
+                                    <input wire:model="nombre" type="text" id="nombre" placeholder="nombre">@error('nombre') <span class="error text-danger">{{ $message }}</span> @enderror
+                                </div>
+                                <div class="field">
+                                    <label>Descripcion <b class="rojo">*</b></label>
+                                    <input wire:model="Descripcion" type="text" id="Descripcion" placeholder="Descripcion">@error('Descripcion') <span class="error text-danger">{{ $message }}</span> @enderror
+                                </div>
+                                <div class="field">
+                                    <label>Añadir tres imagenes <b class="rojo">*</b></label>
+                                    <br>
+                                    <input wire:model="foto" type="file"  id="foto" placeholder="Sube aqui tu foto" multiple accept='image/x-png,image/gif,image/jpg,image/jpeg' />@error('foto') <span class="error text-danger">{{ $message }}</span> @enderror
+                                </div>
+                                <div class="field">
+                                    <label>Estado actual del producto <b class="rojo">*</b></label>
+                                    <br>
+                                    <select wire:model="Estado_actual_del_producto">
+                                        <option value="D">Estado del producto</option>
+                                        <option value="P">Prestado</option>
+                                        <option value="D">Disponible</option>
+                                    </select>
+                                </div>
 
-                </form>
+                                <div class="field">
+                                    <label>Id tipo producto <b class="rojo">*</b></label>
+                                    <br>
+                                    <select wire:model='id_tiposdeproductos' >
+                                        <option value="">Clasificacion de producto</option>
+                                        @foreach($tiposdeproductos as $tiposdeproductos)
+                                        <option value="{{$tiposdeproductos->id}}">{{$tiposdeproductos->clasificacion}} </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div> <!-- modal-body -->
+            <div class="modal-footer">
+
+                <button type="button" class="cancelar" data-dismiss="modal">
+                    Cancelar
+                </button>
+
+                <button type="button" wire:click.prevent="store()" class="btn btn-primary close-modal">Agregar producto</button>
+
             </div>
-
-
         </div>
     </div>
-</body>
-</html>
+</div>
--- prestamo/resources/views/livewire/productossellers/index.blade.php
+++ prestamo/resources/views/livewire/productossellers/index.blade.php
@@ -1,64 +1,43 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Productos disponibles</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <h3>Cosas raras de productos</h3>
+<div class="container">
+    <div class="alert colordark" role="alert">
         <div class="row">
-            <div class="col-xl-12">
-                <form action="" method="get">
-                    <div class="form-row">
-                        <div class="col-auto my-11" >
-                            <a href="Aqui va la direccion" class="btn btn-success">Crea Nuevo</a>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-xl-12">
-                <div class="table-response">
-                    <table class="table-striped">
-                        <thead>
-								<th>Nombre</th>
-								<th>Categoria</th>
-								<th>Descripcion</th>
-								<th>Foto</th>
-                                <th>Foto2</th>
-                                <th>Foto3</th>
-								<th>Estado Actual Del Producto</th>
-								<th>Id Usuario</th>
-								<td>ACTIONS</td>
-                        </thead>
-                        <tbody>
-                        @foreach($productos as $producto)
-							<tr>
-								 
-								<td>{{ $producto->nombre }}</td>
-								<td>{{ $producto->id_tiposdeproductos }}</td>
-								<td>{{ $producto->Descripcion }}</td>
-								<td>{{ $producto->foto }}</td>
-                                <td>{{ $producto->foto2 }}</td>
-                                <td>{{ $producto->foto3 }}</td>
-								<td>{{ $producto->Estado_actual_del_producto }}</td>
-								<td>{{ $producto->id_usuario }}</td>
-								<td >Editar-
-                                    Eliminar
-								</td>
-                            </tr>
-						@endforeach   
-                        </tbody>
-                    </table>
+            <div class="col-lg-5 col-md-5 col-sm-1">
+                <div class="usuario-white">
+                    <b>Productos</b>
                 </div>
 
             </div>
+            <div class="col-lg-5 col-md-5 col-sm-1">
+                @if (session()->has('message'))
+                <div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;"> {{ session('message') }} </div>
+                @endif
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2">
+                <div class="usuario-white">
+                    <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal"> <i class="fas fa-box-open"></i> </i> Agregar Productos</button>
+                </div>
+            </div>
         </div>
     </div>
-    
-</body>
-</html>
+
+
+    <div class="container contenedor">
+
+
+        <center>
+            <div class="row">
+                @foreach($productos as $row)
+                <div class="col-lg-4 col-md-6 col-sm-12 mx-auto">
+                    <div class="card cards" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ $row->foto }}">
+                        <div class="card-body contenido">
+                            <h5 data-toggle="modal" data-target="#updateModal" wire:click="edit({{$row->id}})" class="card-title">{{ $row->nombre }}</h5>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </center>
+    </div>
+
+</div>
