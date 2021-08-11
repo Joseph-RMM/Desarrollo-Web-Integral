513b9ffa20bf3ac60bdf42d8de75b03f50638061
DaddyEdgar <utp0137638@alumno.utpuebla.edu.mx>
2021-08-10 23:36:55 -0500

    Diseño de la campanita, mis amigos y detalles productos

--- prestamo/resources/views/livewire/solicitudes/view.blade.php
+++ prestamo/resources/views/livewire/solicitudes/view.blade.php
@@ -1,74 +1,51 @@
 @section('title', __('Solicitudes'))
-<div class="container-fluid">
-	<div class="row justify-content-center">
-		<div class="col-md-12">
-			<div class="card">
-				<div class="card-header">
-					<div style="display: flex; justify-content: space-between; align-items: center;">
-						<div class="float-left">
-							<div class="container-fluid">
-								@if(session('message'))
-								<div class="row mb-2">
-									<div class="col-lg-12">
-										<div class="alert-danger">
-										{{session('message')}}
-										</div>
-									</div>
-								</div>
-								@endif
-							</div>
-							<h3>Mis solicitudes de amistad y amigos</h3>
-							<h5>A = Aceptado</h5>
-							<h5>P = Pendiente</h5>
-						</div>
-						
-						@if (session()->has('message'))
-						<div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;"> {{ session('message') }} </div>
-						@endif
-
-
-					</div>
-				</div>
-				
-				<div class="card-body">
-						@include('livewire.solicitudes.create')
-						@include('livewire.solicitudes.update')
-				<div class="table-responsive">
-					<table class="table table-bordered table-sm">
-						<thead class="thead">
-							<tr> 
-								<th>Nombre</th>
-								<th>Mensaje</th>
-								<th>Status</th>
-								
-								
-								<td>ACTIONS</td>
-							</tr>
-						</thead>
-						<tbody>
-							@foreach($solicitudes as $row)
-							<tr>
-								<td>{{ $row->name }}</td>
-								<td>{{ $row->Mensaje }}</td>
-								<td>{{ $row->status }}</td>
-								
-								
-								<td width="90">
-								<div class="btn-group">
-
-									
-									<a class="btn btn-success" wire:click="edit({{$row->id}})"  wire:click.prevent="update()"><i class="fa fa-edit"></i> Aceptar</a>							 
-									<a class="btn btn-danger" onclick="confirm('Confirm Delete Solicitude id {{$row->id}}? \nDeleted Solicitudes cannot be recovered!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})"><i class="fa fa-trash"></i> Eliminar </a>   
-									
-								</div>
-								</td>
-							@endforeach
-						</tbody>
-					</table>						
-					{{ $solicitudes->links() }}
-					</div>
+<div class="container">
+	<div class="alert colordark" role="alert">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6">
+				<div class="usuario-white">
+					<b>Mis solicitudes de amistad y amigos</b>
+					<br>
+					<b>A = Aceptado</b>
+					<b>P = Pendiente</b>
 				</div>
 			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6">
+				@if (session()->has('message'))
+				<div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;"> {{ session('message') }} </div>
+				@endif
+			</div>
+
 		</div>
+	</div>
+	<div class="table-responsive">
+		<table class="table table-hover">
+			@include('livewire.solicitudes.create')
+			@include('livewire.solicitudes.update')
+			<thead>
+				<tr>
+					<th scope="col">Nombre</th>
+					<th scope="col">Mensaje</th>
+					<th scope="col">Status</th>
+					<th scope="col">ACTIONS</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($solicitudes as $row)
+				<tr>
+
+					<td>{{ $row->name }}</td>
+					<td>{{ $row->Mensaje }}</td>
+					<td>{{ $row->status }}</td>
+					<td>
+						<button type="button" wire:click="edit({{$row->id}})"  wire:click.prevent="update()" class="button-verde button5"><i class="fas fa-check-circle"></i></i></button>
+						<button type="button" class="button-rojo button5" onclick="confirm('Confirm Delete Solicitude id {{$row->id}}? \nDeleted Solicitudes cannot be recovered!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})"><i class="fas fa-trash-alt"></i></button>
+
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+		{{ $solicitudes->links() }}
 	</div>
 </div>
